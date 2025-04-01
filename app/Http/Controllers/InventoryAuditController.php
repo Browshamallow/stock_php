9d7dd5377018eead9e520d4eec54b5a4a389c5db
Brownshamallow <thiombanendoumbe02@gmail.com>
2025-04-01 02:14:28 +0000

    mise en place de la structuration du projet

---
+++ app/Http/Controllers/InventoryAuditController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InventoryAudit;
+use Illuminate\Http\Request;
+
+class InventoryAuditController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InventoryAudit $inventoryAudit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InventoryAudit $inventoryAudit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, InventoryAudit $inventoryAudit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InventoryAudit $inventoryAudit)
+    {
+        //
+    }
+}
