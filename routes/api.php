cd60a121eb52a90b9026d03ed6d8f26719f579c4
pmf538 <fayepapemoussa9@gmail.com>
2025-04-14 15:02:48 +0200

    Mise en place des supplies

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SupplyController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,12 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/auth/signup', [AuthController::class, 'signup']);
 Route::post('/auth/login', [AuthController::class, 'login']);
+// Routes pour la gestion des approvisionnements 
+Route::middleware('auth:sanctum')->group(function  () {
+    Route::get('/supplies', [SupplyController::class, 'index']);
+    Route::post('/supplies', [SupplyController::class, 'store']);
+    Route::get('/supplies/{supply}', [SupplyController::class, 'show']);
+    Route::put('/supplies/{supply}', [SupplyController::class, 'update']);
+    Route::delete('/supplies/{supply}', [SupplyController::class, 'destroy']);
+});   
+
