4362ea0f057436b954fbd6cd06e22407abec20f1
pmf538 <fayepapemoussa9@gmail.com>
2025-04-15 21:41:22 +0200

    Mise en place des supplies

--- app/Models/Supply.php
+++ app/Models/Supply.php
@@ -1,19 +1,31 @@
 <?php
 
+// Définition du namespace où se trouve le modèle
+
 namespace App\Models;
+
+// Importation des classes nécessaires
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Définition de la classe Supply qui étend le modèle Eloquent de base
+
 class Supply extends Model
 {
+     // Utilisation du trait HasFactory pour permettre la création de factories
+
     use HasFactory;
 
+    // Définition des champs qui peuvent être remplis massivement
+
     protected $fillable = [
-        'product_id',
-        'quantity',
-        'supplier_name'
+        'product_id',   // ID du produit associé
+        'quantity',     // Quantité approvisionnée
+        'supplier_name'   // Nom du fournisseur
     ];
+
+     // Définition de la relation "appartient à" avec le modèle Product
 
     public function product()
     {
